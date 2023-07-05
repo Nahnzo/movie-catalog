664a3faf3c7ac8d6b87f2a0836752984ea51ee31
Dogadaev Alexey <dogadaev142@gmail.com>
2023-07-05 15:21:38 +0300

    add fetch

---
+++ src/Slices/MovieSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getMovie = createAsyncThunk("movie/getMovie", async () => {
+  try {
+    const data = await fetch("https://api.kinopoisk.dev/v1/movie", {
+      method: "GET",
+      headers: {
+        "X-API-KEY": "KNARZC7-GV6MBQC-QY96MPW-RYZFKX5",
+      },
+    });
+    const response = await data.json();
+    return response;
+  } catch (error) {
+    console.log("Error: ", error.message);
+  }
+});
+
+export const movieSlice = createSlice({
+  name: "movie",
+  initialState: {
+    movie: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getMovie.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getMovie.fulfilled, (state, action) => {
+      state.movie = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(getMovie.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default movieSlice.reducer;
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import MovieSlice from "../Slices/MovieSlice";
+export const store = configureStore({
+  reducer: {
+    movie: MovieSlice,
+  },
+});
