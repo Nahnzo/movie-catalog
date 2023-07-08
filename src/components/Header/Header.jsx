ecd5b22eb8451ce87f36605c61cba626f7411b3a
Dogadaev Alexey <dogadaev142@gmail.com>
2023-07-08 15:14:28 +0300

    new files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,12 @@ import styles from "./header.module.css";
 const Header = () => {
   return (
     <section className={styles.header}>
-      <nav>Header</nav>
+      <nav className={styles.navigation}>
+        <h3>Хочу посмотреть</h3>
+        <h3>Моя коллекция</h3>
+        <h3>Что посмотреть?</h3>
+        <h3>Мои рецензии</h3>
+      </nav>
     </section>
   );
 };
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
 import styles from "./movieCard.module.css";
 const MovieCard = ({ data }) => {
-  console.log(data.poster);
+  console.log(data);
   return (
-    <div className={styles.card} style={{ backgroundImage: `url(${data.poster.url})` }}>
-      <h5>{data.alternativeName || data.name}</h5>
+    <div>
+      <div className={styles.card} style={{ backgroundImage: `url(${data.poster.url})` }}></div>
+      <div className={styles.info}>
+        <h3>{data.name || data.alternativeName}</h3>
+        <p>{data.type}</p>
+        <p>{data.year}</p>
+      </div>
     </div>
   );
 };
