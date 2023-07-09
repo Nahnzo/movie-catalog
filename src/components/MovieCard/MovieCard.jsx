8d7c9977063dee1198f1b1b866acb890259b7fa0
Dogadaev Alexey <dogadaev142@gmail.com>
2023-07-09 13:40:53 +0300

    add heart

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+
 /* eslint-disable react/prop-types */
 import styles from "./movieCard.module.css";
 const MovieCard = ({ data }) => {
-  console.log(data);
+  const [showDetails, setShowDetails] = useState(false);
+
+  function addClass(card) {
+    card.target.classList.add(`${styles.hovered}`);
+    setShowDetails(true);
+    console.log(showDetails);
+  }
+  function hideClass(card) {
+    card.target.classList.remove(`${styles.hovered}`);
+    setShowDetails(false);
+    console.log(showDetails);
+  }
   return (
     <div>
-      <div className={styles.card} style={{ backgroundImage: `url(${data.poster.url})` }}></div>
+      <div
+        className={styles.card}
+        style={{ backgroundImage: `url(${data.poster.url})` }}
+        onMouseEnter={(event) => addClass(event)}
+        onMouseLeave={(event) => hideClass(event)}
+      >
+        <div className={styles.details} style={{ display: showDetails ? "block" : "none" }}>
+          <AiOutlineHeart className={styles.heart} />
+        </div>
+      </div>
       <div className={styles.info}>
         <h3>{data.name || data.alternativeName}</h3>
         <p>{data.type}</p>
