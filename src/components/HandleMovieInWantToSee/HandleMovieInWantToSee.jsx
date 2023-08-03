00861527d0108e60c995df7b6eb994d93aa0b401
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-03 14:49:28 +0300

    addHandlerMovieCollection

--- src/components/HandleMovieInWantToSee/HandleMovieInWantToSee.jsx
+++ src/components/HandleMovieInWantToSee/HandleMovieInWantToSee.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addMovieToCollection, removeMovieFromCollection } from "../../Slices/MyCollectionSlice";
+import styles from "./handleMovieInWantToSee.module.css";
 
 const HandleMovieInWantToSee = ({ fMovie }) => {
+  const dispatch = useDispatch();
   let movie = useSelector((data) => data.myCollection.myCollection);
   let phrase = movie.some((item) => item.id === fMovie.id);
+  const handleMovie = (movie) => {
+    phrase ? dispatch(removeMovieFromCollection(movie)) : dispatch(addMovieToCollection(movie));
+  };
 
-  return <button>{phrase ? "Удалить из коллекции" : "Добавить в коллекцию"}</button>;
+  return (
+    <button className={styles.btn} onClick={() => handleMovie(fMovie)}>
+      {phrase ? "Удалить из коллекции" : "Добавить в коллекцию"}
+    </button>
+  );
 };
 
 export default HandleMovieInWantToSee;
