8a00c78cbee6fe35cb74af3c102cfd25b2a9d397
Dogadaev Alexey <dogadaev142@gmail.com>
2023-08-08 19:18:05 +0300

    rev

--- src/components/CardForCollection/CardForCollection.jsx
+++ src/components/CardForCollection/CardForCollection.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addReview, removeMovieFromCollection } from "../../Slices/MyCollectionSlice";
 import styles from "./cardForCollection.module.css";
@@ -8,7 +8,25 @@ import HandleRating from "../HandleRating/HandleRating";
 const CardForCollection = ({ movie }) => {
   const dispatch = useDispatch();
   const [showRateWindow, setShowRateWindow] = useState(false);
+  const [readOnly, setReadOnly] = useState(true);
+  const [initialText] = useState("Место для вашей рецензии");
+  const [review, setReview] = useState("");
+  const refTextArea = useRef(null);
 
+  const leaveRw = () => {
+    dispatch(addReview({ movieId: movie.id, myReviews: review }));
+    setReadOnly((prev) => !prev);
+    if (review == initialText) {
+      setReview("");
+    }
+    refTextArea.current.focus();
+  };
+
+  useEffect(() => {
+    if (readOnly) {
+      setReview(review || initialText);
+    }
+  }, [readOnly]);
   return (
     <div className={styles.cardWrapper}>
       <div className={styles.poster} style={{ backgroundImage: `url(${movie.poster.url})` }}></div>
@@ -18,14 +36,31 @@ const CardForCollection = ({ movie }) => {
           <p>{movie.shortDescription}</p>
           <hr />
           <div className={styles.reviews}>
-            {movie.myReviews ? movie.myReviews : "Место для вашей рецензии "}
+            <textarea
+              className={styles.leaveRw}
+              onChange={(e) => setReview(e.target.value)}
+              readOnly={readOnly}
+              ref={refTextArea}
+              value={readOnly ? movie.myReviews || initialText : review}
+            ></textarea>
           </div>
-          <button
-            className={movie.myRating ? styles.rwBtn : styles.rwBtnAfter}
-            onClick={() => dispatch(addReview({ movieId: movie.id, myReviews: "sss" }))}
-          >
+
+          <button className={movie.myRating ? styles.rwBtn : styles.rwBtnAfter} onClick={leaveRw}>
             {movie.myReviews ? "Изменить резенцию" : "Оставить рецензию"}
           </button>
+          {movie.myReviews.length !== 0 ? (
+            <button
+              className={!movie.myRating ? styles.deleteRw : styles.deleteRwAfter}
+              onClick={() => {
+                setReview("");
+                dispatch(addReview({ movieId: movie.id, myReviews: "" }));
+              }}
+            >
+              Удалить рецензию
+            </button>
+          ) : (
+            ""
+          )}
           <h4>
             {movie.myRating === 0 ? (
               <h5>
