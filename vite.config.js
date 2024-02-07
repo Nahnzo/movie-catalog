b9d52879c58be2d55567943765a4d118f0c22ec3
Alexey <dogadaev142@gmail.com>
2024-02-07 14:00:30 +0300

    versel.json server

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/movie-catalog/",
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: `assets/[name].js`,
+  //       chunkFileNames: `assets/[name].js`,
+  //       assetFileNames: `assets/[name].[ext]`,
+  //     },
+  //   },
+  // },
   resolve: {
     alias: {
       src: "./src",
